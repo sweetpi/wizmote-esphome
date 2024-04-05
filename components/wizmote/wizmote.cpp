e17b20b49b9678ab2bd1137342f53f2246973601
sweet pi - sweet home <sweetpi@users.noreply.github.com>
2024-04-05 13:45:21 +0200

    Update wizmote.cpp

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -4,30 +4,7 @@ namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (update_wizmote_history(&wizmote))
-    return;
-
   this->on_button_->trigger(wizmote);
-}
-
-bool WizMoteListener::update_wizmote_history(WizMotePacket *packet)
-{
-  uint8_t index = 0;
-  if (find_bssid_index(packet, &index))
-  {
-    if (this->history[index].sequence == packet->sequence)
-      return true;
-    
-    // We had a matching MAC, but not a matching sequence, 
-    this->history[index].sequence = packet->sequence;
-  }
-  
-  for (uint8_t i = (WIZMOTEHISTORYSIZE - 1); i > 0; i--)
-    memcpy(&(this->history[i]), &(this->history[i-1]), sizeof(WizMoteHistory));
-  this->history[0].sequence = packet->sequence;
-  memcpy(this->history[0].bssid, packet->bssid, 6);
-  
-  return false;
 }
 
 bool WizMoteListener::find_bssid_index(WizMotePacket *packet, uint8_t *index)
